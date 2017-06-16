54d31db74677a047cce28b9230c596cd601d906e
Paul Arizpe <paul@iterando.mx>
2017-06-16 06:52:35 -0500

    Bing Service

--- Eleonora/Eleonora.Droid/Services/BingService.cs
+++ Eleonora/Eleonora.Droid/Services/BingService.cs
@@ -9,6 +9,8 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace Eleonora.Droid.Services
 {
@@ -16,6 +18,20 @@ namespace Eleonora.Droid.Services
     {
         const string subscriptionKey = "25f95843fc00499e985d68d988e701fe";
         const string uriBase = @"https://westcentralus.api.cognitive.microsoft.com/vision/v1.0";
-        
+
+        static async Task<string> FetchTokenAsync(string search)
+        {
+            var queryString = new Dictionary<string, string>();
+            using (var client = new HttpClient())
+            {
+                queryString.Add("q", search);
+                client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+                UriBuilder uriBuilder = new UriBuilder(uriBase);
+                uriBuilder.Path += "/issueToken";
+
+                var result = await client.PostAsync(uriBuilder.Uri.AbsoluteUri, null);
+                return await result.Content.ReadAsStringAsync();
+            }
+        }
     }
 }
